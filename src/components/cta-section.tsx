78b02529ef58bb04b3d6bca7a7437533033e4cc5
Aanand Madhav <aanand.madhav36@gmail.com>
2025-02-04 20:48:02 +0530

    made cta section responsive on mobile.

--- src/components/cta-section.tsx
+++ src/components/cta-section.tsx
@@ -9,7 +9,7 @@ export function CtaSection() {
   return (
     <section id="cta" className="container">
       <div className="flex items-center rounded-xl bg-primary px-6 py-24 gap-5 flex-row">
-        <div className="flex flex-col gap-5 sm:w-1/2">
+        <div className="flex flex-col gap-5 w-full sm:w-1/2">
           <span className="font-bold uppercase text-background text-left">Let’s Talk</span>
           <h2 className="font-heading text-3xl font-semibold tracking-tight sm:text-4xl text-balance max-w-xl sm:leading-tight text-primary-foreground text-left">
             Get a Custom Quote Today
@@ -19,7 +19,7 @@ export function CtaSection() {
             business.
           </p>
         </div>
-        <div className="sm:w-1/2">
+        <div className="w-full sm:w-1/2">
         <Contact />
           {/* <form className="flex flex-col gap-2 text-background w-full">
             <div>
